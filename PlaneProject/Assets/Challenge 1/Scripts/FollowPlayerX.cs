475a3d527e35587bbdc349f4521c295bbb86f530
Samantha Bast <basts@roverkids.org>
2025-04-10 14:13:52 -0400

    Plane project minus propeller

--- PlaneProject/Assets/Challenge 1/Scripts/FollowPlayerX.cs
+++ PlaneProject/Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -5,16 +5,16 @@ using UnityEngine;
 public class FollowPlayerX : MonoBehaviour
 {
     public GameObject plane;
-    private Vector3 offset;
+    private Vector3 offset = new Vector3(30, 0, 10);
 
     // Start is called before the first frame update
     void Start()
     {
-
+        offset = transform.position - plane.transform.position;
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
         transform.position = plane.transform.position + offset;
     }
